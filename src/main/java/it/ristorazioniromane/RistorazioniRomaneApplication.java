7ed5a75d1c05f090d8f18db8d7eef6880519716c
AMƎLIO <ameliobencich2@gmail.com>
2022-10-02 16:53:58 +0200

    Initial commit

---
+++ src/main/java/it/ristorazioniromane/RistorazioniRomaneApplication.java
@@ -0,0 +1,13 @@
+package it.ristorazioniromane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RistorazioniRomaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RistorazioniRomaneApplication.class, args);
+    }
+
+}
